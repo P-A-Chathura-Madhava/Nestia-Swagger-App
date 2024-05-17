22db72769cda16a328f54b69aa19adb3e21ff7cb
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-05-17 11:20:16 +0530

    Initial commit

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'users'})
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    name: string;
+
+    @Column({type: 'text', nullable: true})
+    address: string;
+
+    @Column({type: 'boolean', default: true})
+    active: boolean;
+}
